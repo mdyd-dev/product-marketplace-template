d011c52f87aac44956484aac6c58f73750d4f64e
Pawel Kowalski <pavelloz@gmail.com>
2020-07-24 22:30:38 +0200

    Setup tailwind, livereload, webpack, etc.

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+const brandColors = {
+  green: '#2b322b',
+};
+
+const systemColors = {
+  blue: '#007aff',
+};
+
+module.exports = {
+  purge: {
+    mode: 'all',
+    content: ['app/**/*.liquid', './src/**/*.liquid', './src/js/**/*.js'],
+  },
+  theme: {
+    extend: {
+      colors: {
+        // `ex` prefix will create classes like `text-ex-red`
+        ex: {
+          ...brandColors,
+          ...systemColors,
+        },
+      },
+      // fontFamily: {
+      //   sans: ['Lato', ...defaultTheme.fontFamily.sans],
+      // },
+      screens: {
+        xxl: '1410px',
+      },
+    },
+  },
+  variants: {
+    borderWidth: ['responsive', 'hover'],
+  },
+  plugins: [require('@tailwindcss/custom-forms')],
+};
---
+++ webpack.config.js
@@ -0,0 +1,68 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+
+const prod = process.env.NODE_ENV === 'production';
+
+const config = {
+  entry: {
+    app: './src/js/app'
+  },
+  output: {
+    filename: 'js/[name].js',
+    chunkFilename: 'js/[name].[chunkhash:3].js',
+    path: path.resolve('app/assets')
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'app'),
+    watchContentBase: true,
+    writeToDisk: true,
+    open: true
+  },
+  bail: true,
+  stats: {
+    assetsSort: '!size',
+    builtAt: false,
+    children: false,
+    modules: false
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader?cacheDirectory'
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader?url=false', 'postcss-loader']
+      }
+    ]
+  },
+  optimization: {
+    minimizer: [
+      new TerserPlugin({
+        parallel: true,
+        cache: true,
+        terserOptions: {
+          output: {
+            comments: false,
+          },
+        },
+        extractComments: false,
+      })
+    ],
+    splitChunks: {
+      cacheGroups: {
+        vendors: false // Do not emit vendors~* files that are almost empty in this setup
+      }
+    }
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].css'
+    })
+  ],
+  mode: prod ? 'production' : 'development'
+};
+
+module.exports = config;
