772569619859b70b5b3e3ac79a1dfbce5d5c0091
damcik <suchuschy@gmail.com>
2020-07-29 17:45:27 +0200

    test

---
+++ tailwind.config.js
@@ -0,0 +1,38 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+const brandColors = {
+  green: '#2b322b',
+};
+
+const systemColors = {
+  blue: '#007aff',
+};
+
+module.exports = {
+  purge: {
+    mode: 'all',
+    content: ['app/**/*.liquid', './src/**/*.liquid', './src/js/**/*.js'],
+  },
+  theme: {
+    container: {
+      center: true,
+      padding: '0'
+    },
+    extend: {
+      colors: {
+        // `ex` prefix will create classes like `text-ex-red`
+        ex: {
+          ...brandColors,
+          ...systemColors,
+        },
+      },
+      // fontFamily: {
+      //   sans: ['Lato', ...defaultTheme.fontFamily.sans],
+      // },
+    },
+  },
+  variants: {
+    borderWidth: ['responsive', 'hover'],
+  },
+  plugins: [require('@tailwindcss/custom-forms')],
+};
