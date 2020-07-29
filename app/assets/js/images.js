9a64bdb8f8c2f1f09c5082a6bacd4d7705422cef
Paweł Kowalski <pavelloz@gmail.com>
2020-07-29 18:03:17 +0200

    Upgrade uppy, cleanup layout a little bit

--- app/assets/js/images.js
+++ app/assets/js/images.js
@@ -4,17 +4,17 @@ const _log = document.querySelector('[data-s3-uppy="log"]');
 const uppy = Uppy.Core({
   autoProceed: true,
   restrictions: {
-    maxFileSize: 2097152,  // Limit size to 2 MB on the javascript side
+    maxFileSize: 2097152, // Limit size to 2 MB on the javascript side
     maxNumberOfFiles: 3,
-    allowedFileTypes: ['image/png', 'image/jpeg', 'image/webp']
-  }
+    allowedFileTypes: ['image/png', 'image/jpeg', 'image/webp'],
+  },
 })
   .use(Uppy.Dashboard, {
     inline: true,
     target: '#drag-drop-area',
-    note: 'Images only, up to 3 files, 2MB each',
+    note: 'Images only, up to 3 files, 2MB each', // Fix, it doesnt work post 1.6.0
     width: '100%',
-    height: 350
+    height: 200
   })
   .use(Uppy.DragDrop)
   .use(Uppy.GoldenRetriever)
@@ -31,29 +31,22 @@ const uppy = Uppy.Core({
       return Promise.resolve({
         method: 'POST',
         url: _url,
-        fields: _fields
+        fields: _fields,
       });
-    }
+    },
   });
-
-uppy.on('upload-success', (_file, data) => {
-  // const li = document.createElement('li');
-  // li.textContent = data.body.location;
-  // _log.appendChild(li);
-});
 
 uppy.on('complete', ({ failed, successful }) => {
   /*
     For every successfully uploaded image to S3, send request to the Instance
     that will create a model with the uploaded image's URL as direct_url param.
   */
-  Promise.all(successful.map(({ response }) => createImage(response.body.location)))
-  .then(() => {
-    alert('ok');
+  Promise.all(successful.map(({ response }) => createImage(response.body.location))).then(() => {
+    console.log('File uploaded and image created!');
   });
 });
 
-const createImage = imageUrl => {
+const createImage = (imageUrl) => {
   // Get logged in user id
   const userId = _form.dataset.s3UppyUserId;
   const itemUuid = _form.dataset.itemUuid;
@@ -63,8 +56,8 @@ const createImage = imageUrl => {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
-      Accept: 'application/json'
+      Accept: 'application/json',
     },
-    body: JSON.stringify({ photo: { direct_url: imageUrl, user_id: userId, item_uuid: itemUuid }})
-  }).then(response => response.json());
+    body: JSON.stringify({ photo: { direct_url: imageUrl, user_id: userId, item_uuid: itemUuid } }),
+  }).then((response) => response.json());
 };
