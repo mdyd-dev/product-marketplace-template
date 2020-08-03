8362bb7356c617b2ed2725c4d2e809567d8bce84
godot <dgorzeba@gmail.com>
2020-08-04 00:43:55 +0200

    unify status interafaces
    use specific property insted of properties
    simplify form [no mentioning properties]

--- test/marketplace_modern_test.js
+++ test/marketplace_modern_test.js
@@ -36,22 +36,25 @@ test(`Login Test`, async (t) => {
   await t
     //listing the item for sale
     .click(Selector('a').withText('List your item'))
-    .typeText('input[name="item[properties][name]"]', item.name) // When labels are configured correctly (and they should, and are now, i think) test should select input by clicking on label - this way it tests label and does not depend on field name which is invisible for user
-    .typeText('input[name="item[properties][type]"]', item.type) // see example: https://github.com/mdyd-dev/pos-cli/blob/7e26a789a76d28649aa10caa528b4a72a65ff4d1/gui/admin/tests/Models.js#L15-L32
-    .typeText('textarea[name="item[properties][description]"]', item.description)
-    .typeText('input[name="item[properties][tags]"]', item.tags)
-    .doubleClick(Selector('main').find('[name="item[properties][price]"]'))
+    .typeText('input[name="item[name]"]', item.name) // When labels are configured correctly (and they should, and are now, i think) test should select input by clicking on label - this way it tests label and does not depend on field name which is invisible for user
+    .typeText('input[name="item[type]"]', item.type) // see example: https://github.com/mdyd-dev/pos-cli/blob/7e26a789a76d28649aa10caa528b4a72a65ff4d1/gui/admin/tests/Models.js#L15-L32
+    .typeText('textarea[name="item[description]"]', item.description)
+    .typeText('input[name="item[tags]"]', item.tags)
+    .doubleClick(Selector('main').find('[name="item[price]"]'))
     .pressKey(clearField)
-    .typeText(Selector('main').find('[name="item[properties][price]"]'), item.price)
-    .click(Selector('main').find('[name="item[properties][cover_photo]"]'))
+    .typeText(Selector('main').find('[name="item[price]"]'), item.price)
+    .click(Selector('main').find('[name="item[cover_photo]"]'))
     .click(Selector('main').find('option').withText('bandage'))
     .click(Selector('main').find('button').withText('browse files'))
     .setFilesToUpload(Selector('main').find('[name="files[]"]'), ['_uploads_/testimage.png'])
     .wait(1000)
     .click(Selector('button[value="create"]')); // button.withText..
+  // assert a browser redirects to item details page
 
   await t
     //searching item by its description
+    .click(Selector('a').withText('MVP Marketplace'))
+    .navigateTo('/')
     .typeText('input[name="k"]', item.description)
     .click(Selector('main').find('button').withText('Search'))
     .click(Selector('main').find('h2 a').withText(item.name))
@@ -59,20 +62,20 @@ test(`Login Test`, async (t) => {
 
   await t
     //change of item information
-    .doubleClick(Selector('main').find('[name="item[properties][name]"]'))
+    .doubleClick(Selector('main').find('[name="item[name]"]'))
     .pressKey(clearField)
-    .typeText('input[name="item[properties][name]"]', editedItem.name)
-    .doubleClick(Selector('main').find('[name="item[properties][type]"]'))
+    .typeText('input[name="item[name]"]', editedItem.name)
+    .doubleClick(Selector('main').find('[name="item[type]"]'))
     .pressKey(clearField)
-    .typeText('input[name="item[properties][type]"]', editedItem.type)
-    .doubleClick(Selector('main').find('[name="item[properties][description]"]'))
+    .typeText('input[name="item[type]"]', editedItem.type)
+    .doubleClick(Selector('main').find('[name="item[description]"]'))
     .pressKey(clearField)
-    .typeText('textarea[name="item[properties][description]"]', editedItem.description)
-    .typeText('input[name="item[properties][tags]"]', editedItem.tags)
-    .doubleClick(Selector('main').find('[name="item[properties][price]"]'))
+    .typeText('textarea[name="item[description]"]', editedItem.description)
+    .typeText('input[name="item[tags]"]', editedItem.tags)
+    .doubleClick(Selector('main').find('[name="item[price]"]'))
     .pressKey(clearField)
-    .typeText(Selector('main').find('[name="item[properties][price]"]'), editedItem.price)
-    .click(Selector('main').find('[name="item[properties][cover_photo]"]'))
+    .typeText(Selector('main').find('[name="item[price]"]'), editedItem.price)
+    .click(Selector('main').find('[name="item[cover_photo]"]'))
     .click(Selector('main').find('option').withText('car'))
     .click(Selector('button[value="update"]'))
     .click(Selector('span').withText('MVP Marketplace'))
