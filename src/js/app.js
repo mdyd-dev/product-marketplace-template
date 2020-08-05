ed343c9d028e63b5cf285c94026b2c47f69fd64e
Paweł Kowalski <pavelloz@gmail.com>
2020-08-05 18:12:33 +0200

    Load uppy where it is needed

--- src/js/app.js
+++ src/js/app.js
@@ -1,5 +1,8 @@
 import '../css/app.css';
 
-import(/* webpackChunkName: "images" */ './images.js').then(() => {
-  console.log('Images module loaded.');
-});
+const uppy = document.querySelector('[data-s3-uppy]');
+if (uppy) {
+  import(/* webpackChunkName: "images" */ './images.js').then(() => {
+    console.log('Images module loaded.');
+  });
+}
