c96884ff130de8cd4fd1b323c4cbb7406186ac7b
damcik <suchuschy@gmail.com>
2020-09-16 14:55:03 +0200

    speed up tests

--- test/pages/newsession.js
+++ test/pages/newsession.js
@@ -4,6 +4,9 @@ export default class NewSessionForm {
   constructor() {
     this.emailInput = Selector('label #email')
     this.passInput = Selector('label #password')
+    this.emailLabel = Selector('label').withText('E-mail')
+    this.passwordLabel = Selector('label').withText('Password')
+    this.usernameLabel = Selector('label[for="username"]')
     this.logInBtn = Selector('button').withText('Log in')
     this.usernameInput = Selector('label #username')
     this.regBtn = Selector('a').withText('Register')
