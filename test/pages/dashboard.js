65e1450a395f432cce8c33498c7f344d7dd28f9e
damcik <suchuschy@gmail.com>
2020-09-21 17:41:36 +0200

    fix tests

--- test/pages/dashboard.js
+++ test/pages/dashboard.js
@@ -2,7 +2,7 @@ import { Selector } from 'testcafe';
 
 export default class DashboardPage {
   constructor() {
-      this.goProfile = Selector('a').withText('Your Profile')
+      this.goProfile = Selector('a').withText('Your Public Profile')
       this.editProfile = Selector('a').withText('Edit Profile')
       this.goYourItems = Selector('main').find('a').withText('Your items')
       this.yourBuyingOrders = Selector('a').withText('Your orders').nth(1)
