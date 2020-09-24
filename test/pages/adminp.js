8f0d88161e6026b1746d7540c49e68fad56121f3
damcik <suchuschy@gmail.com>
2020-09-24 16:46:32 +0200

    group unique name

--- test/pages/adminp.js
+++ test/pages/adminp.js
@@ -7,7 +7,7 @@ export default class AdminPanel {
     this.activities = Selector('a').withText('Activities')
     this.categories = Selector('a').withText('Categories')
     this.orders = Selector('a').withText('Orders')
-    this.items = Selector('a').withText('Items')
+    this.items = Selector('li').find('a').withText('Items')
     this.setup = Selector('a').withText('Setup')
   }
 }
