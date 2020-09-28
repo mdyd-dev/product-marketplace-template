bee367ecc1bee411bcb6c16ffdd270a7f5db9542
godot <dgorzeba@gmail.com>
2020-09-28 19:33:17 +0200

    fix tests

--- test/pages/dashboard.js
+++ test/pages/dashboard.js
@@ -7,7 +7,7 @@ export default class DashboardPage {
       this.goYourItems = Selector('main').find('a').withText('Your items')
       this.yourBuyingOrders = Selector('a').withText('Your orders').nth(1)
       this.yourSellingOrders = Selector('a').withText('Your orders').nth(0)
-      this.activityFeed = Selector('div .flex-grow').find('a').withText('Activity Feed')
+      this.activityFeed = Selector('a').withText('Activity Feed')
       this.yourGroups = Selector('a').withText('Your Groups')
   }
 }
