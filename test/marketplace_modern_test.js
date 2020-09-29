eeeb4d21429e777d3faf68e6a1d709d2cba1ff58
godot <dgorzeba@gmail.com>
2020-09-29 02:22:06 +0200

    fix tests

--- test/marketplace_modern_test.js
+++ test/marketplace_modern_test.js
@@ -1,7 +1,8 @@
 import { Selector, ClientFunction } from 'testcafe'
-import { buyerRole, sellerRole, adminRole, newEmail, newPassword } from './roles'
+import { buyerRole, sellerRole, adminRole, Admin, SellerRandomUser, John } from './roles'
 import { register, myUrl, createItem } from './helper'
 import faker from 'faker'
+
 import NewSessionForm from './pages/newsession'
 import NewItemForm from './pages/newitem'
 import ItemShowPage from './pages/itemshow'
@@ -10,7 +11,7 @@ import ItemSearch from './pages/itemsearch'
 import AdminPanel from './pages/adminp'
 import TopMenuBtns from './pages/topmenu'
 import DashboardPage from './pages/dashboard'
-import profileEditPage from './pages/profileEdit'
+import ProfileEditForm from './pages/profileEdit'
 
 const item = {
   name: faker.commerce.productName(),
@@ -26,7 +27,6 @@ const editedItem = {
   price: '5000',
 }
 
-const newUsername = faker.name.firstName().toLowerCase()
 const loremSentence = (faker.lorem.lines() + " " + faker.lorem.lines())
 const getURL = ClientFunction(() => window.location.href)
 const editURL = '/dashboard/items/edit?id='
@@ -41,17 +41,30 @@ const newItemForm = new NewItemForm()
 const topMenu = new TopMenuBtns()
 const itemSearch = new ItemSearch(item, editedItem)
 const dashboard = new DashboardPage()
-const profileEdit = new profileEditPage()
+const profileEditForm = new ProfileEditForm()
 
-  fixture`Happy path scenario`.page(myUrl)
+fixture`Happy path scenario`.page(myUrl)
 
 test.page(myUrl + '/sign-up')(`Register seller`, async (t) => {
-  await register(newEmail, newPassword, newUsername, 'Tony', 'Montana', true)
+  await register(SellerRandomUser, { required: true })
 })
 
+test.page(myUrl + '/sign-up')(`Register buyer John`, async (t) => {
+  await register(John, { required: false })
+})
 
-test.page(myUrl + '/sign-up')(`Register buyer`, async (t) => {
-  await register('johnsmith@example.com', 'password', 'JohnSmith', 'John', 'Smith', false)
+test('Edit John Profile Page', async (t) => {
+  await t.useRole(buyerRole)
+    .click(topMenu.dashboardBtn)
+    .click(dashboard.editProfile)
+    .typeText(profileEditForm.name, John.name, { replace: true })
+    .typeText(profileEditForm.firstName, John.firstName, { replace: true })
+    .typeText(profileEditForm.lastName, John.lastName, { replace: true })
+    .click(profileEditForm.saveButton)
+    .click(topMenu.dashboardBtn)
+    .click(dashboard.goProfile)
+  await t
+    .expect(Selector('main').find('#user-name').withText(`${John.firstName} ${John.lastName}`).exists).ok()
 })
 
 
@@ -71,26 +84,26 @@ test.page(myUrl + '/sessions/new')(`Trying to register with taken data and log i
     .expect(registerForm.emailLabel.textContent).contains('already taken')
 })
 
-test('Creating item then self follow try', async (t) => {
-    await t.useRole(sellerRole)
-    await createItem(item.name, item.description, item.price)
-    //checks if all data is correct
-    await t.expect(itemShow.name.exists).ok()
-    await t.expect(itemShow.description.exists).ok()
-    await t.expect(itemShow.price.innerText).eql('$10,000', 'check element text')
-    await t.expect('img[src="_uploads_/testimage.png"]').ok()
+test('Creating item then trying to follow', async (t) => {
+  await t.useRole(sellerRole)
+  await createItem(item.name, item.description, item.price)
+  //checks if all data is correct
+  await t.expect(itemShow.name.exists).ok()
+  await t.expect(itemShow.description.exists).ok()
+  await t.expect(itemShow.price.innerText).eql('$10,000', 'check element text')
+  await t.expect('img[src="_uploads_/testimage.png"]').ok()
     .expect(Selector('button').withAttribute('data-follow-user').exists).notOk()
 })
 
 
 test('Editing item and search', async (t) => {
   await t
-    //item search
+  //item search
     .useRole(sellerRole)
     .click(topMenu.dashboardBtn)
     .click(dashboard.goProfile)
-    const sellerProfilePage = ClientFunction(() => document.location.href)
-    await t.expect(sellerProfilePage()).contains(myUrl+'profile/' + newUsername) // checks if href contains slugified username
+  const sellerProfilePage = ClientFunction(() => document.location.href)
+  await t.expect(sellerProfilePage()).contains(myUrl+'profile/' + SellerRandomUser.name) // checks if href contains slugified username
     .click(Selector('a').withText("Items"))  // goes on your list from profile view
     .click(itemSearch.sortButton)
     .click(Selector('option').withText('The Most Recent'))
@@ -105,9 +118,9 @@ test('Editing item and search', async (t) => {
     .typeText(newItemForm.descField, editedItem.description, { replace: true })
     .typeText(newItemForm.priceField, editedItem.price, { replace: true })
     .click(newItemForm.submitBtn)
-    await t.expect(editPage.name.exists).ok()
-    await t.expect(editPage.description.exists).ok()
-    await t.expect(editPage.price.innerText).eql('$5,000', 'check element text')
+  await t.expect(editPage.name.exists).ok()
+  await t.expect(editPage.description.exists).ok()
+  await t.expect(editPage.price.innerText).eql('$5,000', 'check element text')
 })
 
 
@@ -122,16 +135,16 @@ test('Deleting item', async (t) => {
 
 
 test('Creating new item for sell', async (t) => {
-    await t.useRole(sellerRole)
-    await createItem(item.name, item.description, item.price)
+  await t.useRole(sellerRole)
+  await createItem(item.name, item.description, item.price)
 })
 
 
 test('Buying an item and following the seller', async (t) => {
-    await t
+  await t
     .useRole(buyerRole)
     .click(topMenu.itemsBtn)
-    .typeText(itemSearch.searchField, item.name)
+    .typeText(itemSearch.keyword, item.name)
     .click(itemSearch.searchBtn)
     .expect(itemSearch.itemLink.exists).ok()
     .click(itemSearch.itemLink)
@@ -140,14 +153,18 @@ test('Buying an item and following the seller', async (t) => {
     .click(itemShow.buyBtn)
     .click(Selector('button').withText('Checkout'))
     .click(Selector('button').withText('Pay'))
-    await t.click(topMenu.dashboardBtn)
-    .click(dashboard.goProfile)
-    .click(Selector('a').withText('Following'))
-    .expect(Selector('h2').find('a').withText(newUsername).exists).ok("Followed list not shown")
+
     .click(topMenu.dashboardBtn)
     .click(dashboard.yourBuyingOrders) // buyer's order check
     .click(Selector('a').withText(item.name))
-  await t.expect(Selector('div').withText('Ordered').exists).ok()
+    .expect(Selector('div').withText('Ordered').exists).ok()
+
+    .click(topMenu.dashboardBtn)
+    .click(dashboard.goProfile)
+    .click(Selector('a').withText('Following'))
+    .expect(Selector('h2').find('a').withText(SellerRandomUser.name).exists).ok("Followed list not shown")
+
+  await t
     .useRole(sellerRole) // seller checks if his order shown as paid
     .click(topMenu.dashboardBtn)
     .click(dashboard.yourSellingOrders)  // seller's order check
@@ -173,66 +190,54 @@ test(`Admin Panel test`, async (t) => {
   await t.expect(categoriesTableRow.count).gt(1)
     .click(adminPage.activities)
     .click(adminPage.setup)
-  })
+})
 
 
 test('Breakin-in test, edition by none user', async (t) => {
-    await t.useRole(buyerRole)
+  await t.useRole(buyerRole)
     .click(topMenu.itemsBtn)
-    .typeText(itemSearch.searchField, 'Watch')
+    .typeText(itemSearch.keyword, 'Watch')
     .click(itemSearch.searchBtn)
     .click(Selector('a').withText('Watch'))
-    await t
-    var itemEditUrl = await getURL()
-    var itemEditUrl = itemEditUrl.split('-')
-    var editItemId = itemEditUrl[itemEditUrl.length - 1]
-    await t.navigateTo(editURL + editItemId)
-    await t.expect(Selector('div').withText(notAuthorizedUser).exists).ok('message ' + notAuthorizedUser + " doesn't exists")
-})
-
-test('Profile Edit Test', async (t) => {
-  await t.useRole(sellerRole)
-  .click(topMenu.dashboardBtn)
-  .click(dashboard.editProfile)
-  .typeText(profileEdit.firstnameField, 'John Lee', { replace: true })
-  .typeText(profileEdit.lastnameField, 'Hooker', { replace: true })
-  .click(profileEdit.saveButton)
-  .click(topMenu.dashboardBtn)
-  .click(dashboard.goProfile)
-  await t.expect(Selector('main').find('#user-name').withText("John Lee Hooker").exists).ok()
+  await t
+  var itemEditUrl = await getURL()
+  var itemEditUrl = itemEditUrl.split('-')
+  var editItemId = itemEditUrl[itemEditUrl.length - 1]
+  await t.navigateTo(editURL + editItemId)
+  await t.expect(Selector('div').withText(notAuthorizedUser).exists).ok('message ' + notAuthorizedUser + " doesn't exists")
 })
 
 test('Groups', async (t) => {
   const groupName = faker.lorem.words();
   await t.useRole(buyerRole)
-  .click(topMenu.dashboardBtn)
-  .click(dashboard.yourGroups)
-  .click(Selector('a').withText('Add group'))
-  .typeText('#name', groupName)
-  .typeText('#summary', "fun-club")
-  .typeText('#description', loremSentence)
-  .click(Selector('button').withText('Submit'))
-  .expect(Selector('a').withText(groupName).exists).ok()
+    .click(topMenu.dashboardBtn)
+    .click(dashboard.yourGroups)
+    .click(Selector('a').withText('Add group'))
+    .typeText('#name', groupName)
+    .typeText('#summary', "fun-club")
+    .typeText('#description', loremSentence)
+    .click(Selector('button').withText('Submit'))
+    .expect(Selector('a').withText(groupName).exists).ok()
   //unique test
-  .click(topMenu.dashboardBtn)
-  .click(dashboard.yourGroups)
-  .click(Selector('a').withText('Add group'))
-  .typeText('#name', groupName)
-  .typeText('#summary', "fun-club")
+    .click(topMenu.dashboardBtn)
+    .click(dashboard.yourGroups)
+    .click(Selector('a').withText('Add group'))
+    .typeText('#name', groupName)
+    .typeText('#summary', "fun-club")
   await t.debug()
-  .typeText('#description', loremSentence)
-  .click(Selector('button').withText('Submit'))
-  .expect(Selector('div').textContent).contains('already taken')
+    .typeText('#description', loremSentence)
+    .click(Selector('button').withText('Submit'))
+    .expect(Selector('div').textContent).contains('already taken')
 })
 
 test('Activity', async (t) => {
   await t.useRole(buyerRole)
-  .click(topMenu.dashboardBtn)
-  .click(dashboard.activityFeed)
+    .click(topMenu.dashboardBtn)
+    .click(dashboard.activityFeed)
   const feed = ("What's new buddies?")
   await t.typeText(Selector('textarea'), feed)
-  .click(Selector('button').withText('Send'))
-  .click(topMenu.dashboardBtn)
-  .click(dashboard.goProfile)
-  .expect(Selector('div').withText(feed).exists).ok("checks if feed exists at user's profile activities")
+    .click(Selector('button').withText('Send'))
+    .click(topMenu.dashboardBtn)
+    .click(dashboard.goProfile)
+    .expect(Selector('div').withText(feed).exists).ok("checks if feed exists at user's profile activities")
 })
--- test/roles.js
+++ test/roles.js
@@ -2,37 +2,68 @@ import { Selector, Role } from 'testcafe';
 import NewSessionForm from './pages/newsession'
 import faker from 'faker'
 import TopMenuBtns from './pages/topmenu'
+import DashboardPage from './pages/dashboard'
+import ProfileEditForm from './pages/profileEdit'
 
 const topMenu = new TopMenuBtns()
 const loginForm = new NewSessionForm()
 const loginConfirmation = 'Logged in'
-export const newEmail = faker.internet.email().toLowerCase()
-export const newPassword = faker.internet.password()
+const profileEditForm = new ProfileEditForm()
+const dashboard = new DashboardPage()
+
+export const John = {
+  email: 'johnsmith@example.com',
+  password: 'password',
+  name: 'johnsmith',
+  firstName: 'John',
+  lastName: 'Smith'
+}
+
+export const Admin = {
+  email: 'admin@example.com',
+  password: 'password'
+}
+
+const randomFirstName = faker.name.firstName()
+export const SellerRandomUser = {
+  email: faker.internet.email().toLowerCase(),
+  password: faker.internet.password(),
+  name: randomFirstName.toLowerCase(),
+  firstName: randomFirstName,
+  lastName: 'Porter'
+}
 
 const myUrl = process.env.MPKIT_URL
 
 export const buyerRole = Role(myUrl, async (t) => {
   await t
     .click(topMenu.logInBtn)
-    .typeText(loginForm.emailInput, 'johnsmith@example.com')
-    .typeText(loginForm.passInput, 'password')
+    .typeText(loginForm.emailInput, John.email)
+    .typeText(loginForm.passInput, John.password)
     .click(loginForm.logInBtn)
+    .click(topMenu.dashboardBtn)
+    .click(dashboard.editProfile)
+    .typeText(profileEditForm.name, John.name, { replace: true })
+    .typeText(profileEditForm.firstName, John.firstName, { replace: true })
+    .typeText(profileEditForm.lastName, John.lastName, { replace: true })
+    .click(profileEditForm.saveButton)
 })
 
 export const sellerRole = Role(myUrl, async (t) => {
   await t
     .click(topMenu.logInBtn)
-    .typeText(loginForm.emailInput, newEmail)
-    .typeText(loginForm.passInput, newPassword)
+    .typeText(loginForm.emailInput, SellerRandomUser.email)
+    .typeText(loginForm.passInput, SellerRandomUser.password)
     .click(loginForm.logInBtn)
+
   await t.expect(Selector('main').withText(loginConfirmation).exists).ok('message ' + loginConfirmation + " doesn't exists")
 })
 
 export const adminRole = Role(myUrl, async (t) => {
   await t
     .click(topMenu.logInBtn)
-    .typeText(loginForm.emailInput, 'admin@example.com')
-    .typeText(loginForm.passInput, 'password')
+    .typeText(loginForm.emailInput, Admin.email)
+    .typeText(loginForm.passInput, Admin.password)
     .click(loginForm.logInBtn)
 })
 
