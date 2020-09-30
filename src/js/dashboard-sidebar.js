a3966ad512fc972a6cc6f8d61335cae3fa4eb71a
piotrze <piotr.bliszczyk@gmail.com>
2020-09-30 09:11:41 +0200

    Sidebar: new link organization; get rid of js to mark active element

--- src/js/dashboard-sidebar.js
+++
@@ -1,2 +0,0 @@
-const dashboardSidebar = document.querySelector('[data-dashboard-sidebar]');
-dashboardSidebar.querySelector(`a[href='${location.pathname}']`).classList.add("border-b-2","border-blue-700","font-bold");
