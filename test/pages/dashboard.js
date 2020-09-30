f742f1e72732d24d45be95b9496e60a9589b84c9
piotrze <piotr.bliszczyk@gmail.com>
2020-09-30 09:34:46 +0200

    Fix tests after changes in sidebar

--- test/pages/dashboard.js
+++ test/pages/dashboard.js
@@ -4,10 +4,10 @@ export default class DashboardPage {
   constructor() {
     this.activityFeed = Selector('main').find('a').withText('Activity Feed')
     this.editProfile = Selector('a').withText('Edit Profile')
-    this.goProfile = Selector('a').withText('Your Public Profile')
-    this.goYourItems = Selector('main').find('a').withText('Your items')
-    this.yourBuyingOrders = Selector('a').withText('Your orders').nth(1)
-    this.yourSellingOrders = Selector('a').withText('Your orders').nth(0)
-    this.yourGroups = Selector('a').withText('Your Groups')
+    this.goProfile = Selector('a').withText('Public Profile')
+    this.goYourItems = Selector('main').find('a').withText('Items for sell')
+    this.yourBuyingOrders = Selector('a').withText('Purchases').nth(1)
+    this.yourSellingOrders = Selector('a').withText('Sold').nth(0)
+    this.yourGroups = Selector('a').withText('My Groups')
   }
 }
