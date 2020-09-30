c86a04d67829ab351dbf91cc63bd031bd16cd011
piotrze <piotr.bliszczyk@gmail.com>
2020-09-30 10:23:45 +0200

    Fix test after changes in main menu

--- test/pages/topmenu.js
+++ test/pages/topmenu.js
@@ -3,7 +3,8 @@ import { Selector } from 'testcafe';
 export default class TopMenuBtns {
   constructor() {
     this.logoBtn = Selector('span').withText('MVP Marketplace')
-    this.listItemBtn = Selector('a').withText('List your item')
+    this.listItemBtn = Selector('a').withText('Add')
+    this.menuDropdown = Selector('header').find('button').withAttribute('data-menu-dropdown')
     this.dashboardBtn = Selector('a').withText('Dashboard')
     this.logInBtn = Selector('header').find('a').withText('Log in')
     this.logOutBtn = Selector('header').find('button').withText('Log out')
