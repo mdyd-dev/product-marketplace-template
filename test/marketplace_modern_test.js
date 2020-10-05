a48c23c5c6c3a47030515e71e670052b733fe311
damcik <suchuschy@gmail.com>
2020-10-05 11:57:45 +0200

    debug line deleted

--- test/marketplace_modern_test.js
+++ test/marketplace_modern_test.js
@@ -164,7 +164,6 @@ test('Buying an item and following the seller', async (t) => {
     .click(dashboard.yourBuyingOrders) // buyer's order check
     .click(Selector('a').withText(item.name))
     .expect(Selector('div').withText('Ordered').exists).ok()
-    .debug()
     .click(topMenu.menuDropdown)
     .click(topMenu.dashboardBtn)
     .click(dashboard.goProfile)
