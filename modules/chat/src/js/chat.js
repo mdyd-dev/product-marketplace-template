1d006de9031f76c4d04451293cec9aa96f875ab3
Ignacy Moryc <imoryc@gmail.com>
2020-10-22 11:19:31 +0200

    Notifications

--- modules/chat/src/js/chat.js
+++ modules/chat/src/js/chat.js
@@ -1,36 +1,58 @@
 import consumer from "./consumer";
 
-document.addEventListener("DOMContentLoaded", function(){
-  const room = document.getElementById('room');
-  var roomName;
-  var userId;
-  var recipientId;
-  var userName;
+const room = document.getElementById('room');
+var roomName;
+var userId;
+var recipientId;
+var userName;
+const newMessage = document.getElementById('newMessage');
+const newConversationMessage = document.getElementById('new-chat-message');
 
+// channels on profile page
+var senderMessages = null;
+var recipientMessages = null;
+
+// channels on inbox page
+var senderChannel = null;
+var recipientChannel = null;
+
+const inboxMainMessagesId = "main-message-window";
+
+function appendToSenderMessages(data) {
+  const messagesWindow = document.getElementById(inboxMainMessagesId);
+  if (messagesWindow != null) {
+    const message = `
+<div class="flex mb-2 justify-end">
+  <div class="rounded py-2 px-3 bg-indigo-200">
+    <p class="text-sm mt-1"> ${ data["message"] } </p>
+    <p class="text-right text-xs text-gray-500 mt-1"> ${ data["timestamp"] } </p>
+  </div>
+</div>
+`;
+    messagesWindow.insertAdjacentHTML('afterbegin', message);
+  }
+}
+
+
+document.addEventListener("DOMContentLoaded", function(){
   if (room != null) {
     roomName = room.getAttribute('data-room-name');
     userId = room.getAttribute('data-sender-id');
     recipientId = room.getAttribute('data-recipient-id');
     userName = room.getAttribute('data-sender-name');
   }
-  const newMessage = document.getElementById('newMessage');
-  const newConversationMessage = document.getElementById('new-chat-message');
-  var senderMessages = null;
-  var recipientMessages = null;
 
   // for /profile
   if (newMessage != null) {
-
-
     if (senderMessages === null) {
       senderMessages = consumer.subscriptions.create({ channel: "conversate", room_id: roomName, sender_name: userName, from_id: userId, to_id: recipientId, timestamp: new Date() }, {
         received(data) {
           console.log("Recived (sender):", data);
           const notifications = document.getElementById('messages');
           if (notifications != null && data.create == true) {
-            notifications.insertAdjacentHTML('afterbegin', `<div class="flex items-start mb-4 text-sm bg-gray-400" >
+            notifications.insertAdjacentHTML('afterbegin', `<div class="mb-4 text-sm bg-white" >
         <!-- A message -->
-        <div class="flex-1 overflow-hidden border-1 border-gray-600">
+        <div class="flex-1 overflow-hidden">
           <div>
             <span class="font-bold"> ${data.sender_name}</span>
             <span class="text-grey text-xs">${data.timestamp}</span>
@@ -48,12 +70,12 @@ document.addEventListener("DOMContentLoaded", function(){
     if (recipientMessages === null) {
       recipientMessages = consumer.subscriptions.create({ channel: "conversate", room_id: userId }, {
         received(data) {
-          console.log("Recived (recipient):", data);
+          console.log("[Profile] Recived (recipient):", data);
           const notifications = document.getElementById('messages');
 
           if (notifications != null && data.create == true) {
             notifications.insertAdjacentHTML('afterbegin', `
-      <div class="flex items-start mb-4 text-sm bg-gray-400" >
+      <div class="mb-4 text-sm bg-gray-100" >
         <!-- A message -->
         <div class="flex-1 overflow-hidden border-1 border-gray-600">
           <div>
@@ -73,23 +95,16 @@ document.addEventListener("DOMContentLoaded", function(){
     newMessage.addEventListener('keydown', function(event) {
       if (event.keyCode === 13 && userName !== '') {
         const messageData = { message: newMessage.value, from_id: userId, sender_name: userName, to_id: recipientId, timestamp: new Date(), create: true };
-        console.log("Sending..", messageData);
         senderMessages.send(Object.assign(messageData, { create: true  }));
-        console.log("SEnder", senderMessages);
         recipientMessages.send(Object.assign(messageData, { create: false }));
-        console.log("Recipient", recipientMessages);
         newMessage.value = '';
       }
     });
   }
 
-  var senderChannel = null;
-  var recipientChannel = null;
-
   // for /inbox
   if (newConversationMessage != null) {
-
-    console.log("Waiting for new messages");
+    console.log("[Inbox] Waiting for new messages");
 
     const roomName = newConversationMessage.getAttribute('data-from-id');
     const userName = newConversationMessage.getAttribute('data-from-name');;
@@ -109,7 +124,7 @@ document.addEventListener("DOMContentLoaded", function(){
     if (recipientChannel === null) {
       recipientChannel = consumer.subscriptions.create({ channel: "conversate", room_id: recipientId, sender_name: userName, from_id: userId, to_id: recipientId, timestamp: new Date() }, {
         received(data) {
-          console.log("Recived:", data);
+          console.log("[Inbox] Recived:", data);
           const notifications = document.getElementById('messages');
 
           if (notifications != null) {
@@ -146,24 +161,3 @@ document.addEventListener("DOMContentLoaded", function(){
 
 
 });
-
-const inboxMainMessagesId = "main-message-window";
-
-function appendToSenderMessages(data) {
-  const messagesWindow = document.getElementById(inboxMainMessagesId);
-  if (messagesWindow != null) {
-    const message = `
-<div class="flex mb-2 justify-end">
-  <div class="rounded py-2 px-3 bg-indigo-200">
-    <p class="text-sm mt-1"> ${ data["message"] } </p>
-    <p class="text-right text-xs text-gray-500 mt-1"> ${ data["timestamp"] } </p>
-  </div>
-</div>
-`;
-
-    messagesWindow.insertAdjacentHTML('afterbegin', message);
-
-
-  }
-
-}
--- modules/chat/src/js/notifications.js
+++ modules/chat/src/js/notifications.js
@@ -1,29 +1,24 @@
 import consumer from "./consumer";
 
-document.addEventListener("DOMContentLoaded", function(){
-  const inbox = document.getElementById('inbox-notifications');
-  const current_user_id = inbox.getAttribute('data-current-user-id');
 
-  console.log("Setup notifications for user #", current_user_id);
-
-  consumer.subscriptions.create({ channel: "conversate", room_id: current_user_id }, {
-    received(data) {
-      if (data.to_id === current_user_id || data.from_id == current_user_id) {
-        document.getElementById('notificationsBell').style.display = "block";
-        if (window.location.pathname.startsWith("/inbox") && data.to_id == current_user_id) {
-          const room = document.getElementById('new-chat-message');
-          const sentTo = room.getAttribute('data-to-id');
-          if (sentTo == data.from_id) {
-            appendToRecipientMessages(data);
-          }
-        }
-      }
-    }
-  });
-});
-
-
+const inbox = document.getElementById('inbox-notifications');
+const current_user_id = inbox.getAttribute('data-current-user-id');
 const inboxMainMessagesId = "main-message-window";
+
+function notification(sender, message) {
+  if (!("Notification" in window)) {
+    console.log("This browser does not support desktop notification");
+  } else if (Notification.permission === "granted") {
+    var notification = new Notification(`${sender}: ${message}`);
+  } else if (Notification.permission !== "denied") {
+    Notification.requestPermission().then(function (permission) {
+      if (permission === "granted") {
+        var notification = new Notification(`${sender}: ${message}`);
+      }
+    });
+  }
+}
+
 function appendToRecipientMessages(data) {
   const messagesWindow = document.getElementById(inboxMainMessagesId);
   if (messagesWindow != null) {
@@ -39,3 +34,26 @@ function appendToRecipientMessages(data) {
   }
 
 }
+
+document.addEventListener("DOMContentLoaded", function(){
+  console.log("Setup notifications for user #", current_user_id);
+
+  consumer.subscriptions.create({ channel: "conversate", room_id: current_user_id }, {
+    received(data) {
+      if (data.to_id === current_user_id || data.from_id == current_user_id) {
+        document.getElementById('notificationsBell').style.display = "block";
+        if (data.to_id == current_user_id) {
+          notification(data.sender_name, data.message);
+        }
+        if (window.location.pathname.startsWith("/inbox") && data.to_id == current_user_id) {
+          const room = document.getElementById('new-chat-message');
+          const sentTo = room.getAttribute('data-to-id');
+          if (sentTo == data.from_id) {
+            appendToRecipientMessages(data);
+          }
+        }
+      }
+    }
+  });
+});
+
