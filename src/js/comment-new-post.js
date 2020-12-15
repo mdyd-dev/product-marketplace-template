fe02b413e4e03833ee2021c8f5d2883bab44e33e
piotrze <piotr.bliszczyk@gmail.com>
2020-12-15 10:55:22 +0100

    Questions: add comments

--- src/js/comment-new-post.js
+++ src/js/comment-new-post.js
@@ -2,3 +2,12 @@ const newCommentPost = document.querySelector('[data-comment-new-post]');
 newCommentPost.querySelector('[data-comment-photo-upload]').addEventListener('click', function () {
   newCommentPost.querySelector('#drag-drop-area').classList.toggle('hidden');
 });
+
+const showNewComments = document.querySelectorAll('[data-comment-show-new-post]');
+showNewComments.forEach((showComment)=> {
+  showComment.onclick = (e) => {
+    const button = e.currentTarget;
+    button.classList.toggle('hidden');
+    button.closest('[data-comment-new-post]').querySelector('[data-comment-form]').classList.toggle('hidden')
+  };
+});
