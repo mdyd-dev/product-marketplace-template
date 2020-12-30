a34a94b784f6ffb638b9cb644cc0e89881841081
damcik <suchuschy@gmail.com>
2020-12-30 19:20:25 +0100

    add comment test

--- test/pages/topicsPage.js
+++ test/pages/topicsPage.js
@@ -27,7 +27,7 @@ export default class TopicsPage {
     }
     this.ratings = {
       question: Selector('span[data-tc="votes-count"]').withText('1').nth(0),
-      firstAnswer: Selector('main').find('div').find('span').withText('1').nth(1)
+      firstAnswer: Selector('span[data-tc="votes-count"]').withText('1').nth(1)
     }
   }
 }
