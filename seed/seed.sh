6133efe5d7d3421230ddeb785260f7cb52229753
piotrze <piotr.bliszczyk@gmail.com>
2021-01-05 13:58:50 +0100

    Fix

--- seed/seed.sh
+++ seed/seed.sh
@@ -10,6 +10,6 @@ cp categories-i18n.en.txt ../app/translations/en_categories.yml
 cd ..
 
 pos-cli data clean --auto-confirm --include-schema
-pos-cli deploy -o
+pos-cli deploy
 pos-cli data import --path=./seed/data.zip --zip
 pos-cli uploads upload --path=seed/images
